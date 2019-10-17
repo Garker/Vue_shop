d79f8eabea79c858df07a820f41aed9e12ab5668
darker <200571164@qq.com>
2019-10-17 17:09:36 +0800

    分类参数完成

--- src/router.js
+++ src/router.js
@@ -7,51 +7,54 @@ import Users from './components/user/Users.vue'
 import Rights from './components/power/Rights.vue'
 import Roles from './components/power/Roles.vue'
 import Cate from './components/goods/Cate.vue'
-
+import Params from './components/goods/Params.vue'
 Vue.use(Router)
 
 const router = new Router({
-    routes: [{
-            path: '/',
-            redirect: '/login'
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        {
+          path: '/welcome',
+          component: Welcome
         },
         {
-            path: '/login',
-            component: Login
+          path: '/users',
+          component: Users
         },
         {
-            path: '/home',
-            component: Home,
-            redirect: '/welcome',
-            children: [{
-                    path: '/welcome',
-                    component: Welcome
-                },
-                {
-                    path: '/users',
-                    component: Users
-                },
-                {
-                    path: '/rights',
-                    component: Rights
-                },
-                {
-                    path: '/roles',
-                    component: Roles
-                },
-                { path: '/categories', component: Cate }
-            ]
-        }
-    ]
+          path: '/rights',
+          component: Rights
+        },
+        {
+          path: '/roles',
+          component: Roles
+        },
+        { path: '/categories', component: Cate },
+        { path: '/params', component: Params }
+      ]
+    }
+  ]
 })
 
 // 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
-    if (to.path == '/login') return next()
+  if (to.path === '/login') return next()
 
-    // 获取token
-    const tokenStr = window.sessionStorage.getItem('token')
-    if (!tokenStr) return next('/login')
-    next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 export default router
